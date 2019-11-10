d1d01c15246b679bc9e72c0bf5c8a555ccc74466
Vanderley <vpibm502@gmail.com>
2019-11-10 01:10:44 -0300

    Modifying HelloWord app

--- src/basica/HelloWorld.java
+++ src/basica/HelloWorld.java
@@ -3,6 +3,7 @@ package basica;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("I´m adding code to my project");
 	}
 
 }
